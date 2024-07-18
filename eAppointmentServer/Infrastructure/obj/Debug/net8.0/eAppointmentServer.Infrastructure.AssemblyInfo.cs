4dc16fc959c6458ed15d5e765da3e1f27ec22677
Serkan Öztürk <serkanoztrk237@gmail.com>
2024-07-18 15:41:13 +0300

    Patient CRUD operations added - fullname problem fixed (backend)

--- eAppointmentServer/Infrastructure/obj/Debug/net8.0/eAppointmentServer.Infrastructure.AssemblyInfo.cs
+++ eAppointmentServer/Infrastructure/obj/Debug/net8.0/eAppointmentServer.Infrastructure.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eAppointmentServer.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ff43db46966f6b459410226f2643fdea11f04b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8310e196a522e523b98185bd86bf304a63b746b")]
 [assembly: System.Reflection.AssemblyProductAttribute("eAppointmentServer.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eAppointmentServer.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
